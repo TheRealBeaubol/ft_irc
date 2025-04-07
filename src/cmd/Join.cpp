82a74e4b066bcabc67b9b31e69af946c734b476a
Malo Hervoche <mhervoch@3F1.42angouleme.fr>
2025-04-07 23:58:42 +0200

    patch join (invite only check)

--- src/cmd/Join.cpp
+++ src/cmd/Join.cpp
@@ -6,7 +6,7 @@
 /*   By: lboiteux <lboiteux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 00:39:23 by lboiteux          #+#    #+#             */
-/*   Updated: 2025/04/07 22:45:27 by mhervoch         ###   ########.fr       */
+/*   Updated: 2025/04/07 23:58:29 by mhervoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	joinCommand(Server *server, Client *client, std::vector<std::string> commands) {
 	
-	std::string	msg_ERR;
+	std::string	msg_R;
 	std::string	serverName = std::string(SERVER_NAME);
 	if (commands.size() < 2)
 	{
-		msg_ERR = serverName + " 461 " + client->getNickName();
-		send(client->getClientSocket(), msg_ERR.c_str(), msg_ERR.size(), 0);
-		std::cout << BOLD RED << msg_ERR << RESET;
+		msg_R = serverName + " 461 " + client->getNickName();
+		send(client->getClientSocket(), msg_R.c_str(), msg_R.size(), 0);
+		std::cout << BOLD RED << msg_R << RESET;
 		return ;
 	}
 	size_t 	channelSize = server->getChannel().size();
@@ -31,42 +31,44 @@ void	joinCommand(Server *server, Client *client, std::vector<std::string> comman
 	{
 		if (tmpChannel->getClients().size() > (unsigned long) tmpChannel->getClientLimit())
 		{
-			msg_ERR = serverName + " 471 " + client->getNickName();
-			send(client->getClientSocket(), msg_ERR.c_str(), msg_ERR.size(), 0);
-			std::cout << BOLD RED << msg_ERR << RESET;
+			msg_R = serverName + " 471 " + client->getNickName();
+			send(client->getClientSocket(), msg_R.c_str(), msg_R.size(), 0);
+			std::cout << BOLD RED << msg_R << RESET;
 			return ;
 		}
 		if (tmpChannel->getClientByName(client->getNickName()) != NULL)
 		{
-			msg_ERR = serverName + " 443 " + client->getNickName();
-			send(client->getClientSocket(), msg_ERR.c_str(), msg_ERR.size(), 0);
-			std::cout << BOLD RED << msg_ERR << RESET;
+			msg_R = serverName + " 443 " + client->getNickName();
+			send(client->getClientSocket(), msg_R.c_str(), msg_R.size(), 0);
+			std::cout << BOLD RED << msg_R << RESET;
 			return ;
 		}
-		if (tmpChannel->getInviteOnly() == true && tmpChannel->getClients()[client][0] == false){
-			msg_ERR = serverName + " 473 " + client->getNickName();
-			send(client->getClientSocket(), msg_ERR.c_str(), msg_ERR.size(), 0);
-			std::cout << BOLD RED << msg_ERR << RESET;
+		if (tmpChannel->getInviteOnly() == true && tmpChannel->getClientByName(client->getNickName()) && tmpChannel->getClients()[client][0] == false){
+			msg_R = serverName + " 473 " + client->getNickName();
+			send(client->getClientSocket(), msg_R.c_str(), msg_R.size(), 0);
+			std::cout << BOLD RED << msg_R << RESET;
 			return ;
 		}
 		if (tmpChannel->getPassword().empty() == false){
 		
-			msg_ERR = serverName + " 475 " + client->getNickName();
+			msg_R = serverName + " 475 " + client->getNickName();
 			if (commands.size() < 3){
-				send(client->getClientSocket(), msg_ERR.c_str(), msg_ERR.size(), 0);
-				std::cout << BOLD RED << msg_ERR << RESET;
+				send(client->getClientSocket(), msg_R.c_str(), msg_R.size(), 0);
+				std::cout << BOLD RED << msg_R << RESET;
 				return ;
 			}
 			if(tmpChannel->getPassword() != commands[2]){
-				send(client->getClientSocket(), msg_ERR.c_str(), msg_ERR.size(), 0);
-				std::cout << BOLD RED << msg_ERR << RESET;
+				send(client->getClientSocket(), msg_R.c_str(), msg_R.size(), 0);
+				std::cout << BOLD RED << msg_R << RESET;
 				return ;
 			}
 		}
 		std::cout << LIGHTMAGENTA << "Channel [ " << tmpChannel->getChannelName() << " ] exists, user joined it" << RESET << std::endl;
 		tmpChannel->addClient(client);
+		tmpChannel->getClients()[client][1] = true;
 		server->sendMessage(client->getClientSocket(), msg.c_str());
-		
+		msg_R = ":server 332 " + client->getNickName() + " #" + tmpChannel->getChannelName() + " :" + tmpChannel->getTopic() + "\r\n";
+		send(client->getClientSocket(), msg_R.c_str(), msg_R.size(), 0);
 	}
 	else
 	{
